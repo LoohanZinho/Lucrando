d2463674faf90ea9aef0a18d65037f697bebb32b
Lohan Santos <lohansantosborges@gmail.com>
2025-09-08 17:44:53 +0000

    deixe a nossa barra de navegação do sistema bonita, no globalccss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,8 +90,8 @@ export default {
           },
         },
          'toast-progress': {
-          '0%': { transform: 'scaleX(1)' },
-          '100%': { transform: 'scaleX(0)' },
+          '0%': { transform: 'scaleX(0)' },
+          '100%': { transform: 'scaleX(1)' },
         },
       },
       animation: {
