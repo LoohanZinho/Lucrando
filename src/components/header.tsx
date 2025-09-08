2e6d81d50778fa8c822fd0e8d15ffdac794fe844
Lohan Santos <lohansantosborges@gmail.com>
2025-09-08 17:21:22 +0000

    inicia com o laranja, e termina com o branco

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
     return (
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <div className="flex-1">
-                 <h1 className="truncate text-lg font-semibold bg-gradient-to-r from-foreground to-primary bg-clip-text text-transparent">Lucrando com Influenciadores</h1>
+                 <h1 className="truncate text-lg font-semibold bg-gradient-to-r from-primary to-foreground bg-clip-text text-transparent">Lucrando com Influenciadores</h1>
             </div>
             <div className="relative ml-auto flex items-center gap-2 md:grow-0">
                 <ThemeToggle />
