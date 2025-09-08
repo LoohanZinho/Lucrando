44ba9300a9757dc1f512f8cd259cd09f52db4de9
Lohan Santos <lohansantosborges@gmail.com>
2025-09-08 17:52:37 +0000

    no celular o cabeçalho sumiu! nao tá aparecendo em nenhuma pagina e nem

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,6 +6,7 @@ import { useAuth } from '@/contexts/auth-context';
 import { Sidebar } from '@/components/sidebar';
 import { useLoader } from '@/contexts/loader-context';
 import { BottomNav } from '@/components/bottom-nav';
+import { Header } from '@/components/header';
 
 export default function AppLayout({
   children,
@@ -36,6 +37,7 @@ export default function AppLayout({
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <Sidebar />
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        <Header />
         <main className="flex-1 items-start gap-4 p-4 sm:px-6 md:gap-8 pb-20 sm:pb-4">
             {children}
         </main>
