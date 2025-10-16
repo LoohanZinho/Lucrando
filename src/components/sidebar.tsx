0d1c8a90333ddace3d3d3ab0f4c45c4cd79992cb
Lohan Santos <lohansantosborges@gmail.com>
2025-10-16 19:08:18 +0000

    deixe a logo maior

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -19,8 +19,8 @@ export function Sidebar() {
     return (
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
             <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-                <Link href="#" className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
-                    <Image src="https://i.imgur.com/bgXDxQU.png" alt="LCI Logo" width={24} height={24} />
+                <Link href="#" className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-10 md:w-10 md:text-base">
+                    <Image src="https://i.imgur.com/bgXDxQU.png" alt="LCI Logo" width={32} height={32} />
                     <span className="sr-only">LCI</span>
                 </Link>
                 <TooltipProvider>
