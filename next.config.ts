863f9072efa251ae24223f7bce3ecdcd02a41aa4
Lohan Santos <lohansantosborges@gmail.com>
2025-10-16 17:32:07 +0000

    Access to manifest at 'https://6000-firebase-studio-1757340415932.cluste

--- next.config.ts
+++ next.config.ts
@@ -42,6 +42,19 @@ const nextConfig = {
       }
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(nextConfig);
