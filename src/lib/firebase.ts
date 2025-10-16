a5cd4601125465cc638d10a66a653e468735dfbb
Lohan Santos <lohansantosborges@gmail.com>
2025-10-16 15:28:40 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCstZaRIU1hyIzjGvktfeuhbmX0HSwk

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,16 +1,17 @@
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 // We keep getAuth in case it's needed for other Firebase services, but it's not used for login.
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCstZaRIU1hyIzjGvktfeuhbmX0HSwkTM0",
+  authDomain: "lci-dash.firebaseapp.com",
+  projectId: "lci-dash",
+  storageBucket: "lci-dash.firebasestorage.app",
+  messagingSenderId: "627350653326",
+  appId: "1:627350653326:web:60089372f6f44da5ffbcdf",
+  measurementId: "G-S4ZEW74QBL"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
