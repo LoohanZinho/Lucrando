9f49f73b4ca644899fe12d5058f5745da47f2d8a
Lohan Santos <lohansantosborges@gmail.com>
2025-10-16 19:07:42 +0000

    nao nao!!, to me referindo a ele no sidebar (_for element <LinkComponent

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -19,7 +19,7 @@ export function Sidebar() {
     return (
         <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
             <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-                <Link href="#" className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
+                <Link href="#" className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
                     <Image src="https://i.imgur.com/bgXDxQU.png" alt="LCI Logo" width={24} height={24} />
                     <span className="sr-only">LCI</span>
                 </Link>
